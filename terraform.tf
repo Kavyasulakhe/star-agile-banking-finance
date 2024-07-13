9a6630c52e259cd98af7bca68d387dba4e8bad2e
Kavyasulakhe <78170271+Kavyasulakhe@users.noreply.github.com>
2024-07-13 23:04:05 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -125,7 +125,7 @@ resource "aws_eip" "proj-eip" {
 
 
 # Creating an ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
+resource "aws_instance" "test-Server" {
  ami = "ami-0ef82eeba2c7a0eeb"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1b"
@@ -139,6 +139,6 @@ resource "aws_instance" "Prod-Server" {
      sudo apt-get update -y
  EOF
  tags = {
- Name = "Prod-Server"
+ Name = "test-Server"
  }
 }
