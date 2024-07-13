8f7e7e59c0cf9faf981d92edfef855145e706d19
Kavyasulakhe <78170271+Kavyasulakhe@users.noreply.github.com>
2024-07-13 23:08:18 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -126,7 +126,7 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "test-Server" {
- ami = "ami-0ef82eeba2c7a0eeb"
+ ami = "ami-0c2af51e265bd5e0e"
  instance_type = "t2.micro"
  availability_zone = "ap-south-1b"
  key_name = "karuna"
